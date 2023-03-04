f70430bcfd2a64d80a70b6365d4706ace414fda5
Davi Alexandre <deltreertl@gmail.com>
2023-03-03 22:06:42 -0300

    painel de ações

---
+++ views/refactor/ListarContatos.java
@@ -0,0 +1,33 @@
+package views;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class ListarContatos extends JFrame {
+  
+  //PANELS
+  public FormPanel panelForm;
+  public ActionsPanel actionsPanel;
+
+  public ListarContatos() {
+    Container container = this.getContentPane();
+    panelForm = new FormPanel();
+    actionsPanel = new ActionsPanel(panelForm);
+
+    container.setLayout(new BorderLayout());
+    container.add(panelForm.panel, BorderLayout.CENTER);
+    container.add(actionsPanel.panel, BorderLayout.PAGE_END);
+  
+    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    this.setResizable(false);
+    this.pack();
+  }
+
+  public void fecharJanela(){
+    this.setVisible(false);
+    this.dispose();
+  }
+}
