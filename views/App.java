4a0ab3520a9af644b1aae99e69a94df137e12658
RodrigoGama-4 <62919032+RodrigoGama-4@users.noreply.github.com>
2023-03-03 22:38:11 -0300

    Update App.java

--- views/App.java
+++ views/App.java
@@ -5,7 +5,7 @@ import javax.swing.SwingUtilities;
 public class App{
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() ->{
-            Login login = new Login();
+            ListarContatos login = new ListarContatos();
             login.setVisible(true);
         }
         );
